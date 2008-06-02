be1a7a52a28a8b3dbc1609693bf10536f9fc10a0
David Clifford <David.Clifford@csiro.au>
2008-06-02 00:00:00 +0000

    version 1.0-1

---
+++ src/extra.h
@@ -0,0 +1,7 @@
+static inline double sqr(const double x) {return(x*x);}
+
+static inline int abs(const int x) {return((x<0)?(-x):x);}
+
+static inline int max(const int x, const int y) {return((x<y)?y:x);}
+
+
